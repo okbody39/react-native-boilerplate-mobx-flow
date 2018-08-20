b7cae49b396ff96f43b803cc93dc47c454b4880a
한재용 <>
2018-08-20 19:06:46 +0900

    no message

--- src/stories/screens/Overview/index.js
+++ src/stories/screens/Overview/index.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Container, Header, Title, Content, Text, Button, Icon, Left, Right, Body } from "native-base";
 
+import PureChart from 'react-native-pure-chart';
+
 import styles from "./styles";
 export interface Props {
 	navigation: any;
@@ -9,7 +11,60 @@ export interface State {}
 class Overview extends React.Component<Props, State> {
 	render() {
 		const param = this.props.navigation.state.params;
-		return (
+    let sampleData = [
+      {
+        seriesName: 'series1',
+        data: [
+          {x: '2018-02-01', y: 30},
+          {x: '2018-02-02', y: 200},
+          {x: '2018-02-03', y: 170},
+          {x: '2018-02-04', y: 250},
+          {x: '2018-02-05', y: 10},
+          {x: '2018-02-06', y: 10},
+          {x: '2018-02-07', y: 10},
+          {x: '2018-02-08', y: 10},
+          {x: '2018-02-09', y: 10},
+          {x: '2018-02-10', y: 10},
+          {x: '2018-02-11', y: 10}
+        ],
+        color: '#297AB1'
+      },
+      {
+        seriesName: 'series2',
+        data: [
+          {x: '2018-02-01', y: 20},
+          {x: '2018-02-02', y: 100},
+          {x: '2018-02-03', y: 140},
+          {x: '2018-02-04', y: 550},
+          {x: '2018-02-05', y: 40},
+          {x: '2018-02-06', y: 20},
+          {x: '2018-02-07', y: 20},
+          {x: '2018-02-08', y: 20},
+          {x: '2018-02-09', y: 20},
+          {x: '2018-02-10', y: 20},
+          {x: '2018-02-11', y: 20}
+        ],
+        color: 'green'
+      }
+    ];
+    let sampleData2 = [
+      {
+        value: 50,
+        label: 'Marketing',
+        color: 'red',
+      }, {
+        value: 40,
+        label: 'Sales',
+        color: 'blue'
+      }, {
+        value: 25,
+        label: 'Support',
+        color: 'green'
+      }
+
+    ];
+
+    return (
 			<Container style={styles.container}>
 				<Header>
 					<Left>
@@ -26,7 +81,10 @@ class Overview extends React.Component<Props, State> {
 				</Header>
 
 				<Content padder>
-					<Text>{param !== undefined ? param.name.item.caption : "Create Something Awesome . . ."}</Text>
+					{/*<Text>{param !== undefined ? param.name.item.caption : "Create Something Awesome . . ."}</Text>*/}
+          <PureChart data={sampleData} type='line' height={200} />
+          <PureChart data={sampleData} type='bar' height={200} />
+          <PureChart data={sampleData2} type='pie' height={200} />
 				</Content>
 			</Container>
 		);
